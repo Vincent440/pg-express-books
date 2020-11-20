4eed8e410e10d5fd0395cfbc30cf2337d8e988c5
vince s <vinceshury@gmail.com>
2020-11-20 17:55:40 -0500

    Connected node.js server with react client and added scripts. start for production, dev for development

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const app = express()
 
 const routes = require('./routes')
 const PORT = process.env.PORT || 3001
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.use(routes)
 
 app.listen(PORT, () =>
-  console.log(`pg-express-books server running on\nhttp://localhost:${PORT}`)
+  console.log(`pg-express-books API server running on:\nhttp://localhost:${PORT}`)
 )
