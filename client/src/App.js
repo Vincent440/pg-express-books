a3fe261a58643eba5c1055de87640188c9f7c402
vince s <vinceshury@gmail.com>
2020-11-20 18:41:04 -0500

    Reverted script, formatted App.js and added logging of books

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect} from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { useEffect } from 'react'
 
-function App() {
-  useEffect(() =>{
-    console.log("App loaded")
-    fetch('/api/?search=Avis+Lang').then(res => res.json().then(data => console.log(data)))
+function App () {
+  useEffect(() => {
+    console.log('App loaded')
+    fetch('/api/?search=Avis+Lang')
+      .then(res => res.json().then(data => console.log(data)))
+      .catch(errMsg => console.err(errMsg))
   })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Check the console!
+        </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
