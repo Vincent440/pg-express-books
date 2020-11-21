f2ca61abe5e68db9a642fc3c83d707437d4c7eb7
vince s <vinceshury@gmail.com>
2020-11-21 17:21:49 -0500

    Displaying default books API query on page load

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg'
 import './App.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function App () {
+  const [books, setBooks] = useState([])
+
   useEffect(() => {
     console.log('App loaded')
     fetch('/api/?search=Avis+Lang')
-      .then(res => res.json().then(data => console.log(data)))
+      .then(res =>
+        res.json().then(data => {
+          setBooks(data)
+          console.log(books)
+          return
+        })
+      )
       .catch(errMsg => console.error(errMsg))
-  })
+  }, [])
+
   return (
     <div className='App'>
       <header className='App-header'>
+        <h1>PG Express Books</h1>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Check the console!
-        </p>
         <a
           className='App-link'
           href='https://reactjs.org'
@@ -27,6 +31,27 @@ function App () {
         >
           Learn React
         </a>
+        <div>
+          <ul>
+            {books.length === 0 ? (
+              <li>No books to display.</li>
+            ) : (
+              books.map(book => {
+                return (
+                  <li>
+                    <h2>Title: {book.title}</h2>
+                    <p>
+                      Description: {book.description || ''}
+                    </p>
+                    <p>
+                      Authors: {book.authors.join(', ')}
+                    </p>
+                  </li>
+                )
+              })
+            )}
+          </ul>
+        </div>
       </header>
     </div>
   )
